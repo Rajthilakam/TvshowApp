3d11739d682e249c8f353560e5f0ae0da6996bd0
rajthilakam <rajthilakam@gmail.com>
2021-06-15 16:01:59 -0700

    Navbar fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BackgroundImageComponent } from './background-image/background-image.component';
+import { SeasonsComponent } from './seasons/seasons.component';
+import { EpisodesComponent } from './episodes/episodes.component';
+import { GenreComponent } from './genre/genre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    BackgroundImageComponent
+    BackgroundImageComponent,
+    SeasonsComponent,
+    EpisodesComponent,
+    GenreComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
